509f0c8a80546ffcc37d3e7f4fdb4afe05ae47ac
jayStackk <jmagpantay@codestack.co>
2023-10-19 21:19:55 -0700

    quick fixx

--- src/components/pokedex/index.js
+++ src/components/pokedex/index.js
@@ -29,7 +29,7 @@ export default function Pokedex(props) {
       </div>
 
       {loading ? (
-        <div className="hold">Carregando mais pokemons... </div>
+        <div className="hold">Loading more pokemons... </div>
       ) : (
         <div className="pokedex-grid">
           {pokemons &&
