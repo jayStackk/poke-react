6feba74077dd9c15f1afc6bb974b7d33dbc20cc8
jayStackk <jmagpantay@codestack.co>
2023-10-19 21:13:24 -0700

    Publish Pokedex in react using styled components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getPokemonData, getPokemons, searchPokemon } from "./api";
+import { FavoriteProvider } from "./components/context/pokecontext";
+import Navbar from "./components/navbar";
+import Searchbar from "./components/searchbar";
+import Pokedex from "./components/pokedex";
+import Imagez from "./assets/pokedexx.jpg"
 
+const favoritesKey = "f";
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const itensPerPage = 24;
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      setNotFound(false);
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage));
+    } catch (error) {
+      console.log("fetchPokemons error:", error);
+    }
+  };
+
+  const loadFavoritePokemons = () => {
+    const pokemons =
+      JSON.parse(window.localStorage.getItem(favoritesKey)) || [];
+    setFavorites(pokemons);
+  };
+
+  useEffect(() => {
+    loadFavoritePokemons();
+  }, []);
+
+  useEffect(() => {
+    fetchPokemons();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const updateFavoritePokemons = (name) => {
+    const updatedFavorites = [...favorites];
+    const favoriteIndex = favorites.indexOf(name);
+    if (favoriteIndex >= 0) {
+      updatedFavorites.splice(favoriteIndex, 1);
+    } else {
+      updatedFavorites.push(name);
+    }
+
+    window.localStorage.setItem(favoritesKey, JSON.stringify(updatedFavorites));
+    setFavorites(updatedFavorites);
+  };
+
+  const onSearchHandler = async (pokemon) => {
+    if (!pokemon) {
+      return fetchPokemons();
+    }
+
+    setLoading(true);
+    setNotFound(false);
+    const result = await searchPokemon(pokemon);
+    if (!result) {
+      setNotFound(true);
+    } else {
+      setPokemons([result]);
+      setPage(0);
+      setTotalPages(1);
+    }
+    setLoading(false);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FavoriteProvider
+      value={{
+        favoritePokemons: favorites,
+        updateFavoritePokemons: updateFavoritePokemons,
+      }}
+    >
+     <div style={{
+  backgroundImage: `url(${Imagez})`,
+  backgroundSize: 'cover', // Cover the entire container
+  backgroundRepeat: 'no-repeat', // Do not repeat the image
+  minHeight: '120vh', // Ensure the div covers the entire viewport height
+}}>
+        <Navbar />
+        <Searchbar onSearch={onSearchHandler} />
+        {notFound ? (
+          <h1>Não achamos esse Pokemon :\</h1>
+        ) : (
+          <Pokedex
+            pokemons={pokemons}
+            loading={loading}
+            page={page}
+            setPage={setPage}
+            totalPages={totalPages}
+          />
+        )}
+      </div>
+    </FavoriteProvider>
   );
 }
 
---
+++ src/components/searchbar/styled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 20px 0px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: black;
+
+  font-family: "Mulish";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  
+  input {
+  padding: 11px;
+  border-radius: 2px;
+  border: 1px solid var(--white-200);
+  border-radius: 10px;
+  color: black;
+  cursor: pointer;
+}
+
+    ::placeholder {
+    color: black;
+    opacity: 1; /* Firefox */
+  }
+
+button {
+  background-color: #0e6f9f;
+  border: none;
+  border-radius:10px;
+  height: 40px;
+  color: var(--white);
+  padding: 10px 12px;
+  margin-left:12px;
+  cursor: pointer;
+}`  
